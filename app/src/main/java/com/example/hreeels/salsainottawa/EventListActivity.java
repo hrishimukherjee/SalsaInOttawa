7bf50c10552931dbbf41290545782d4127c3f249
Hreeels <hrishileo@hotmail.com>
2015-11-30 01:38:47 -0500

    Event List Chooses Right Layout Based On List
    EventListActivity chooses the right layout based on if the event list is
    empty or not.

--- app/src/main/java/com/example/hreeels/salsainottawa/EventListActivity.java
+++ app/src/main/java/com/example/hreeels/salsainottawa/EventListActivity.java
@@ -26,6 +26,7 @@ public class EventListActivity extends ActionBarActivity {
     // GUI Elements
     private TextView iSearchResultTitle;
     private ListView iSearchResultList;
+    private TextView iNoSearchResultsText;
 
     // Data Attributes
     private ArrayList<Event> iEventList; /* The list of events to be displayed on the screen */
@@ -33,10 +34,6 @@ public class EventListActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_event_list);
-
-        // Initialize the components of the screen
-        initializeViewComponents();
 
         // Retrieve the query results from the intent
         iEventList = getIntent().getParcelableArrayListExtra("queryResult");
@@ -47,13 +44,31 @@ public class EventListActivity extends ActionBarActivity {
 
     /**
      * Sets up the activity with the provided list of events.
+     * It also chooses the right view to display depending on
+     * if the event list is empty or not.
      *
-     * @param aEventList
+     * @param aEventList the list of events to display
      */
     public void setupActivity(ArrayList<Event> aEventList) {
-        decorateComponents();
+        System.out.println(aEventList.size());
 
-        activateActionListeners();
+        int lLayoutID;
+
+        // Choose the right layout depending on if the list is empty or not
+        if(aEventList.size() != 0) {
+            lLayoutID = R.layout.activity_event_list;
+        } else {
+            lLayoutID = R.layout.activity_event_list_empty;
+        }
+
+        setContentView(lLayoutID);
+
+        // Initialize the components of the screen
+        initializeViewComponents(lLayoutID);
+
+        decorateComponents(lLayoutID);
+
+        activateActionListeners(lLayoutID);
 
         updateEventList(aEventList);
     }
@@ -61,37 +76,46 @@ public class EventListActivity extends ActionBarActivity {
     /**
      * Initializes the view components of the activity.
      */
-    public void initializeViewComponents() {
+    public void initializeViewComponents(int aLayoutID) {
         iSearchResultTitle = (TextView) findViewById(R.id.search_result_title);
-        iSearchResultList = (ListView) findViewById(R.id.theListView);
+
+        if(aLayoutID == R.layout.activity_event_list) {
+            iSearchResultList = (ListView) findViewById(R.id.theListView);
+        } else if(aLayoutID == R.layout.activity_event_list_empty) {
+            iNoSearchResultsText = (TextView) findViewById(R.id.no_events_text);
+        }
     }
 
-    public void decorateComponents() {
+    public void decorateComponents(int aLayoutID) {
         Typeface lCustomFont = Typeface.createFromAsset(getAssets(), "bebas_neue_regular.ttf");
 
         iSearchResultTitle.setTypeface(lCustomFont);
+
+        if(aLayoutID == R.layout.activity_event_list_empty) {
+            iNoSearchResultsText.setTypeface(lCustomFont);
+        }
     }
 
     /**
      * Activates all the action listeners for the components in the activity.
      */
-    public void activateActionListeners() {
-        iSearchResultList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Event eventPicked = (Event) parent.getItemAtPosition(position);
+    public void activateActionListeners(int aLayoutID) {
+        if(aLayoutID == R.layout.activity_event_list) {
+            iSearchResultList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Event eventPicked = (Event) parent.getItemAtPosition(position);
 
-                String displayString = "You picked " +
-                        eventPicked.getTitle();
+                    String displayString = "You picked " +
+                            eventPicked.getTitle();
 
-                // Set up an intent, pass in the query results, and start a new activity
-                Intent myIntent = new Intent(EventListActivity.this, EventDetails.class);
-                myIntent.putExtra("eventPicked", eventPicked);
-                EventListActivity.this.startActivity(myIntent);
-
-                // Toast.makeText(EventListActivity.this, displayString, Toast.LENGTH_SHORT).show();
-            }
-        });
+                    // Set up an intent, pass in the query results, and start a new activity
+                    Intent myIntent = new Intent(EventListActivity.this, EventDetails.class);
+                    myIntent.putExtra("eventPicked", eventPicked);
+                    EventListActivity.this.startActivity(myIntent);
+                }
+            });
+        }
     }
 
     /**
@@ -101,9 +125,11 @@ public class EventListActivity extends ActionBarActivity {
      * @param aEventList the new event list to display
      */
     public void updateEventList(ArrayList<Event> aEventList) {
-        // Initialize the Array Adapter with the event list
-        ArrayAdapter theAdapter = new EventAdapter(this, aEventList);
+        if(aEventList.size() != 0) {
+            // Initialize the Array Adapter with the event list
+            ArrayAdapter theAdapter = new EventAdapter(this, aEventList);
 
-        iSearchResultList.setAdapter(theAdapter);
+            iSearchResultList.setAdapter(theAdapter);
+        }
     }
 }
