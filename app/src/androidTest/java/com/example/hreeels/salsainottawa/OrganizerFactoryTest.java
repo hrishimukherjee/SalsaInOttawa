3da0b6f4a46677b874142869beb6c6e7a8018fb6
Hreeels <hrishileo@hotmail.com>
2015-11-30 03:51:20 -0500

    Unit Tests for Venue & Organizer Factory

---
+++ app/src/androidTest/java/com/example/hreeels/salsainottawa/OrganizerFactoryTest.java
@@ -0,0 +1,29 @@
+package com.example.hreeels.salsainottawa;
+
+import com.example.hreeels.salsainottawa.core.Organizer;
+import com.example.hreeels.salsainottawa.factory.OrganizerFactory;
+import com.parse.ParseObject;
+
+import junit.framework.Assert;
+
+/**
+ * Created by Hreeels on 2015-11-30.
+ */
+public class OrganizerFactoryTest extends ApplicationTest {
+
+    public void testCreateFromParseObject() {
+        ParseObject lParseObject = ParseObject.create("organizer");
+
+        lParseObject.put("organizer_name", "Test Organizer");
+        lParseObject.put("organizer_phone_number", "123-456-7890");
+        lParseObject.put("organizer_email", "test@email.com");
+        lParseObject.put("organizer_website", "www.test.com");
+
+        Organizer lOrganizer = OrganizerFactory.createOrganizer(lParseObject);
+
+        Assert.assertEquals(lOrganizer.getTitle(), "Test Organizer");
+        Assert.assertEquals(lOrganizer.getPhoneNumber(), "123-456-7890");
+        Assert.assertEquals(lOrganizer.getEmail(), "test@email.com");
+        Assert.assertEquals(lOrganizer.getWebsite(), "www.test.com");
+    }
+}
